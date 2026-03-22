e538b3aa9fbad1831010650d02312f1cf2ba5ac2
ehildt <ehildt@users.noreply.github.com>
2026-03-22 19:35:31 +0000

    BADGE BUMP

--- dist/cache-return-value/index.d.ts
+++ dist/cache-return-value/index.d.ts
@@ -4,6 +4,6 @@ interface CacheConfig<T = unknown> {
     schema?: Joi.Schema<T>;
     ttl?: number | false;
 }
-declare function CacheReturnValue<T = unknown>(config?: Joi.Schema<T> | CacheConfig<T> | false): MethodDecorator & PropertyDecorator;
+declare function CacheReturnValue<T = unknown>(config?: Joi.Schema<T> | CacheConfig<T> | false | number): MethodDecorator & PropertyDecorator;
 
 export { type CacheConfig, CacheReturnValue };
